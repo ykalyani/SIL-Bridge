776a601eb16dc82fdc2bd0504561a4cc8b92e1ea
smayoorans <smayoorans@gmail.com>
2013-11-30 11:40:48 +0530

    Add Student profile

--- sil-bridge/sil-web/src/main/java/org/sil/bridge/controller/LecturerController.java
+++ sil-bridge/sil-web/src/main/java/org/sil/bridge/controller/LecturerController.java
@@ -1,5 +1,14 @@
 package org.sil.bridge.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LecturerController {
+
+    @RequestMapping(value = "/student/profile")
+    public String dashBoardPage(){
+        return "/student/profile";
+    }
 }
--- sil-bridge/sil-web/src/main/java/org/sil/bridge/controller/StudentController.java
+++ sil-bridge/sil-web/src/main/java/org/sil/bridge/controller/StudentController.java
@@ -1,5 +1,13 @@
 package org.sil.bridge.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class StudentController {
+
+    @RequestMapping(value = "/profile")
+    public String dashBoardPage(){
+        return "profile";
+    }
 }
