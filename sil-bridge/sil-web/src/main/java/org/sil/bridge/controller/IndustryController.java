6fa81dc950c303a69b3d09b08f0ec10fcceadccf
Kirishanthy <kirishanthy.t@gmail.com>
2013-11-30 17:07:36 +0530

    Added homepage

--- sil-bridge/sil-web/src/main/java/org/sil/bridge/controller/IndustryController.java
+++ sil-bridge/sil-web/src/main/java/org/sil/bridge/controller/IndustryController.java
@@ -1,5 +1,14 @@
 package org.sil.bridge.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndustryController {
+
+    @RequestMapping(value = "/industry/home")
+    public String studentHomePage(){
+        return "industry/home";
+    }
 }
